18d8cbb37f251f4dc5e079c2c26fa58e170f5c43
Felicia Ullstad <felicia.ullstad@gmail.com>
2017-02-21 15:43:43 +1300

    Added comments to Leo's script

--- techtronix4040.py
+++ techtronix4040.py
@@ -2,8 +2,12 @@ import telnetlib
 import time
 import datetime
 
+#b'+9.90000000E+37\r\n'
+#+9.90000000E+37
+#This is the open circuit value for any measurements
+#Set multimeter to DHCP and hard reset. Might have to change the IP to closer to your computer IP.
 
-multimeter_address ="192.168.1.1"
+multimeter_address ="169.254.112.87"
 #setup telnet
 multimeter = telnetlib.Telnet()
 #initialize the multimeter with the given settings
@@ -15,8 +19,9 @@ multimeter.write(("SYST:REM\n").encode('ascii'))
 print('start :  {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
 try:
     for i in range(60):
-        multimeter.write(("MEAS:VOLT:DV?\n").encode("ascii"))
+        multimeter.write(("MEAS:VOLT:DC?\n").encode("ascii"))
         voltage= multimeter.read_eager()
+        print(voltage)
         voltage = voltage.decode("ascii")
         print(voltage)
         time.sleep(1)
